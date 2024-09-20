7d33e6a047e94923c552ac1b2aebae38732e01ee
eduardogarcia2012@hotmail.com <garciaeduardo2012@hotmail.com>
2024-09-19 19:10:58 -0500

    cambio de mi proyeto

--- script.js
+++ script.js
@@ -2,29 +2,57 @@ let cart = [];
 let cartCount = 0;
 let totalPrice = 0;
 
+
+// addToCart()
+
 function addToCart(product, price) {
     cart.push({ product, price });
     cartCount++;
     totalPrice += price;
+    console.log(cart);
+    
     
 
     document.getElementById('cart-count').textContent = cartCount;
 
   
     let cartItems = document.getElementById('cart-items');
-    let newItem = document.createElement('li');
+    let newItem = document.createElement('li')
+    let buttonEliminar = document.createElement('button')
+    buttonEliminar.textContent = 'X'
+    buttonEliminar.classList.add('btn','btn-primary','eliminar');
     newItem.textContent = `${product} - $${price}`;
     cartItems.appendChild(newItem);
+    cartItems.appendChild(buttonEliminar)
 
+    const elimiminar = document.querySelector('.eliminar') 
+    elimiminar.addEventListener('click',eliminarMoto)
     
     document.getElementById('total-price').textContent = totalPrice;
 
-    alert(`${product} agregado al carrito!`);
 }
-boton.parentNode.removeChild(boton.parentNode);
+//  boton.parentNode.removeChild(boton.parentNode);
 
 function toggleCart() {
     const cartPopup = document.getElementById('cart-popup');
     cartPopup.style.display = cartPopup.style.display === 'block' ? 'none' : 'block';
 }
 
+
+function eliminarMoto(e){
+    console.log('en el eliminar');
+    const li = e.target.nextSibling
+    const {product, price } = cart
+    console.log(li);
+   console.log(cart);
+       
+    cartCount--
+    totalPrice-=price
+ console.log(cart);
+ console.log(cartCount);
+ 
+ 
+  cart = [...cart]
+  console.log(cart);
+  
+}
